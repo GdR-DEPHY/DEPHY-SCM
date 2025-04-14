3bb0b811a52f15f9e769fc359d0694a775feb72e
Najda Villefranque <najda.villefranque@gmail.com>
2025-04-14 14:51:55 +0200

    new strategy to handle subcases + fix driver_SCM
    e.g.
    
    gen_REF/ instead of REF/ for generic drivers
    REFXXX instead of 3days/RUNXXX for instantiated cases
    
    + try/catch error in thermo.py
    + gitignore poub/ and logs/

--- BOTANY/make_cases.sh
+++ BOTANY/make_cases.sh
@@ -1,31 +1,21 @@
 set -e
+mkdir -p logs
+
 sim=SHORT
 sim=REF
 
-rm -f RUN*
-
 cd .. && source setenv && cd - > /dev/null 2>&1
 
-for f in input_files/profiles_member*$1*txt
+for f in input_files/profiles_member*$1.txt
 do
   ncase=$(echo $f|sed -e "s/^.*prof.*member_//g" -e "s/.txt//g")
-  cas=RUN$ncase
+  cas=$sim$ncase
   echo $cas
   mkdir -p $cas
   cp $f $cas/profiles_init.txt
   cp input_files/profiles_tendencies.txt $cas
   cd $cas
-  python ../$sim/driver_DEF.py -n $cas -p
+  python ../gen_$sim/driver_DEF.py -n $cas -p -v     >> ../logs/log_driver_DEF_$$ 2>&1
+  python ../gen_$sim/driver_SCM.py -n $cas -p -c -v  >> ../logs/log_driver_SCM_$$ 2>&1
   cd - > /dev/null 2>&1
 done
-
-case $sim in 
-  SHORT) dir=1day;;
-  REF) dir=3days;;
-esac
-
-test -d $dir && mv $dir /tmp/${dir}_$$
-
-mkdir -p $dir
-mv -f RUN* $dir
-ln -sf $dir/RUN* .
